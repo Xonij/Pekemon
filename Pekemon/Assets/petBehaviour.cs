549533f8b75a49e5d367bef1fd81075188c3929f
Niilo <then1ppe@gmail.com>
2018-01-25 14:46:53 +0200

    asd

---
+++ Pekemon/Assets/petBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class petBehaviour : MonoBehaviour {
+
+    public GameObject player;
+    public float distToPlayer;
+
+    void Start () {}
+	void Update ()
+    {
+        distToPlayer = Vector3.Distance(this.transform.position, player.transform.position);
+        moveToPlayer();
+        hideWhenInAttack();
+    }
+    void moveToPlayer()
+    {
+        if (distToPlayer > 3.5f)
+        {
+            this.gameObject.transform.position = Vector2.MoveTowards(gameObject.transform.position, player.transform.position, Time.deltaTime * 3f);
+        }
+    }
+    void hideWhenInAttack()
+    {
+        if (player.GetComponent<playerAttacking>().atkAllow == true) { this.gameObject.GetComponentInChildren<SpriteRenderer>().enabled = true; }
+        else { this.gameObject.GetComponentInChildren<SpriteRenderer>().enabled = false; }
+    }
+}
---
+++ Pekemon/Assets/playerMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    public float speed,speedDefault = 2.7f;
+
+	void Start () {
+		
+	}
+	void Update () {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            speed = speedDefault * 2f;
+        }
+        else { speed = speedDefault; }
+
+        moving(); 	
+	}
+    public void moving()
+    {
+        if (GetComponent<playerAttacking>().atkAllow == true)
+        {
+            if (Input.GetButton("up") || Input.GetKey(KeyCode.UpArrow))
+            {
+                GetComponent<playerAttacking>().DirectionFacing = directionFacing.up;
+                transform.Translate(Vector3.up * speed * Time.deltaTime);
+            }
+            if (Input.GetButton("down") || Input.GetKey(KeyCode.DownArrow))
+            {
+                GetComponent<playerAttacking>().DirectionFacing = directionFacing.down;
+                transform.Translate(Vector3.down * speed * Time.deltaTime);
+            }
+            if (Input.GetButton("left") || Input.GetKey(KeyCode.LeftArrow))
+            {
+                GetComponent<playerAttacking>().DirectionFacing = directionFacing.left;
+                transform.Translate(Vector3.left * speed * Time.deltaTime);
+            }
+            if (Input.GetButton("right") || Input.GetKey(KeyCode.RightArrow))
+            {
+                GetComponent<playerAttacking>().DirectionFacing = directionFacing.right;
+                transform.Translate(Vector3.right * speed * Time.deltaTime);
+            }
+        }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == "wall")
+        {
+            Debug.Log("dfghjkl");
+        }
+        if (other.gameObject.tag == "Enemy")
+        {
+           // this.gameObject.GetComponent<playerAttacking>().playerHealth--;
+        }
+    }
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Enemy"&&GetComponent<playerAttacking>().playerHealth>0)
+        {
+            this.gameObject.GetComponent<playerAttacking>().playerHealth--;
+        }
+    }
+}
