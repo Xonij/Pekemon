1c3cacc668861d10ba5ceafcd65e0b3cf58d195d
Xonij <joni.sil94@gmail.com>
2018-02-06 16:29:26 +0200

    Player facing code progressed

--- Assets/Scriptit/npc.cs
+++ Assets/Scriptit/npc.cs
@@ -13,11 +13,16 @@ public class npc : MonoBehaviour {
     public GameObject npcTextBox,screenCanvas;
     public bool seenThisNpc=false;
 
+    protected float angle;
+
     void Start ()
     {	
 	}
 	void Update ()
     {
+        if(distToPlayer<4)
+        lookToPlayerSide();
+
         distToPlayer = Vector3.Distance(this.transform.position, player.transform.position);
 
         if (useAltTextBox == false)
@@ -48,5 +53,25 @@ public class npc : MonoBehaviour {
                 screenCanvas.GetComponentInChildren<Text>().text = "";
             }
         }
+    }
+    public void lookToPlayerSide()
+    {
+        Vector2 player_relative_to_enemy = this.gameObject.transform.position - player.transform.position;
+        player_relative_to_enemy.Normalize();
+        angle = Mathf.Atan2(player_relative_to_enemy.x, player_relative_to_enemy.y);
+        /*
+        if(angle>2f && angle > -2.2f) { Debug.Log("up"); }
+        else if (angle < 1f && angle > -1f) { Debug.Log("down"); }
+        else if (angle > 1f && angle < 3f) { Debug.Log("left"); }
+        else if (angle > -2.25f && angle < -0.75f) { Debug.Log("right"); }
+        */
+        if ((angle < (-Mathf.PI/4)*3 && angle > -Mathf.PI)||(angle > (Mathf.PI / 4) * 3 && angle < Mathf.PI))
+        { Debug.Log("up"); }
+        if (angle > (-Mathf.PI / 4) && angle < (Mathf.PI / 4))
+        { Debug.Log("down"); }
+        if (angle > (Mathf.PI / 4) && angle < (Mathf.PI / 4) * 3)
+        { Debug.Log("left"); }
+        if ((angle > (-Mathf.PI / 4) * 3 && angle < (-Mathf.PI / 4)))
+        { Debug.Log("right"); }
     }
 }
